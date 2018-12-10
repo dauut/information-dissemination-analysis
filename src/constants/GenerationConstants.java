3e0c80088ba68327570fec1fef145d8cc6c341b4
dauut <davutucar@gmail.com>
2018-12-09 20:40:47 -0800

    Generation Constants New Friends Creation

---
+++ src/constants/GenerationConstants.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class GenerationConstants {
+
+    public static final long AMOUNT_OF_NEW_USER_ID = 50000;
+
+    public static long getAmountOfNewUserId() {
+        return AMOUNT_OF_NEW_USER_ID;
+    }
+}
--- src/fileio/CreateActivity.java
+++ src/fileio/CreateActivity.java
@@ -1,5 +1,6 @@
 package fileio;
 
+import constants.GenerationConstants;
 import structure.UserInformations;
 
 import java.io.IOException;
@@ -10,14 +11,18 @@ public class CreateActivity {
     public static void main(String[] args) {
         ArrayList<UserInformations> usersList;
         ReadData getUserFromData = new ReadData();
-
+        CreateActivity ca = new CreateActivity();
+        ArrayList<Long> generatedUserIDs = new ArrayList<>();
+        long longestUserID;
 
         try {
             usersList = getUserFromData.getUserList();
+            longestUserID = ca.getLargestUserID(usersList);
+            generatedUserIDs = ca.userIdGenerator(longestUserID);
+
             int friendsCount = 0;
             for (UserInformations u : usersList) {
                 friendsCount = u.getAllOnlineFriends().size();
-
 
             }
         } catch (IOException e) {
@@ -28,17 +33,33 @@ public class CreateActivity {
 
     }
 
-    public long getLargestUserID(ArrayList<UserInformations> usersList) {
+    private long getLargestUserID(ArrayList<UserInformations> usersList) {
         long longestUserID = 0;
 
-        for (UserInformations u : usersList){
-            for (int i = 0; i < u.getAllOnlineFriends().size(); i++){
+        for (UserInformations u : usersList) {
+            ArrayList<Long> userAllFriends;
+            userAllFriends = new ArrayList<>(u.getAllOnlineFriends());
 
+            for (int i = 0; i < userAllFriends.size(); i++) {
+                if (userAllFriends.get(i) > longestUserID) {
+                    longestUserID = userAllFriends.get(i);
+                }
             }
         }
 
-
+        System.out.println("Longest User ID in Dataset = " + longestUserID);
         return longestUserID;
+    }
+
+    private ArrayList<Long> userIdGenerator(long startId) {
+        ArrayList<Long> newUserIds = new ArrayList<>();
+
+        for (long i = startId; i < startId + GenerationConstants.getAmountOfNewUserId(); i++) {
+            newUserIds.add(i);
+        }
+
+        return newUserIds;
+
     }
 
 }
